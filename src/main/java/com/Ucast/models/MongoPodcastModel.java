e484cb7c8b2b4fb65f8b46f5670c5ce5bdc1fbb4
Vladyslav Bozhko <drakosvlad@gmail.com>
2021-04-23 09:38:40 +0300

    add listened field

--- src/main/java/com/Ucast/models/MongoPodcastModel.java
+++ src/main/java/com/Ucast/models/MongoPodcastModel.java
@@ -24,6 +24,11 @@ public class MongoPodcastModel {
     private boolean checked;
     private String description;
     private String photoURL;
+
+    public int getListened() {
+        return listened;
+    }
+
     private int listened;
     private double rating;
 //    @JsonIgnore
@@ -58,6 +63,7 @@ public class MongoPodcastModel {
         description = that.getDescription();
         photoURL = that.getPhotoURL();
         timecodes = that.getTimecodes();
+        listened = that.getListened();
     }
 
     @Override
